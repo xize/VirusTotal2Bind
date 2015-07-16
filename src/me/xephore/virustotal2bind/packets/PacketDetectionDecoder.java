507d58b1a225c2218b575d4e92eb6b7a7cf8bece
xize <glucassen@live.nl>
2015-07-16 16:22:30 +0200

    fix NullPointerException

--- src/me/xephore/virustotal2bind/packets/PacketDetectionDecoder.java
+++ src/me/xephore/virustotal2bind/packets/PacketDetectionDecoder.java
@@ -4,29 +4,29 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class PacketDetectionDecoder implements Decoder {
-	
+
 	private final JSONObject obj;
 	private int detections = 0;
-	
+
 	public PacketDetectionDecoder(JSONObject obj) {
 		this.obj = obj;
 	}
-	
+
 	@Override
 	public void decode() {
 		JSONArray array = (JSONArray) obj.get("detected_urls");
-		
-		String name = array.toString().replace("{", "").replace("}", "").replace("[", "").replace("]", "");
-		
-		String[] args = name.split(",");
-		
-		for(String a : args) {
-			if(a.contains("total")) {
-				this.detections = (this.detections+Integer.parseInt(a.replace("\"total\":", "")));
+
+		if(array != null) {
+			String name = array.toString().replace("{", "").replace("}", "").replace("[", "").replace("]", "");
+			String[] args = name.split(",");
+			for(String a : args) {
+				if(a.contains("total")) {
+					this.detections = (this.detections+Integer.parseInt(a.replace("\"total\":", "")));
+				}
 			}
 		}
 	}
-	
+
 	@Override
 	public Object getResult() {
 		return "detections: " + detections;
